0ae7b0dfe67cdce6470635369e926738c7f3481f
Pure-Heart <ph@Tyrens-MacBook-Pro.local>
2021-08-12 20:32:45 -0400

    ditch Django, its too schwerig

--- api/retro_gamer/n64/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class n64ListSerializer(serializers.Serializer):
-    list = serializers.JSONField()
-
-    # @action write_to_json
--- api/retro_gamer/n64/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import serializers
-
-urlpatterns = [
-    path('', serializers.n64ListSerializer ),
-]
